7a1400dbd2a6f39c7caf09eb4d9af70f2aa4276b
rokibulslame <rokibulslam.programmer@gmail.com>
2021-09-26 20:01:10 +0600

    style addded

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="mb-5 pt-5">
+        <div className="mb-5 pt-5 header">
             <h1>Build Your Own Software Company with Best Software Developer</h1>
             <h5>Hire expert Software Developers for any job</h5>
             <h1>Total Budget: 1.5 Million</h1>
